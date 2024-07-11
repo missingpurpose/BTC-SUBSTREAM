f62f0ad35d4e0258caa035cbf8afd626449ef0ca
Bal <balwydcomms@gmail.com>
2024-07-11 11:08:36 -0400

    subgraph yaml, lib.rs map_tx update

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use pb::bitcoin::template::Txo;
+use pb::bitcoin::template::{Txo, Transaction, Address};
 use substreams;
 use substreams_bitcoin::pb::btc::v1::Block;
 mod pb;
@@ -6,22 +6,54 @@ mod pb;
 #[substreams::handlers::map]
 pub fn map_tx(block: Block) -> Result<Txo, substreams::errors::Error> {
     let mut txo_count = 0;
+    let mut transactions = vec![];
+    let mut address_map = std::collections::HashMap::new();
 
     for tx in block.tx {
         substreams::log::println(format!("Tx Hash: {}", tx.hash));
-        for vin in tx.vin {
+        let mut tx_value = 0.0;
+
+        for vin in &tx.vin {
             substreams::log::println(format!("Coinbase: {}", &vin.coinbase));
             substreams::log::println(format!("Linked Prev Tx: {}", &vin.txid));
             substreams::log::println(format!("Vout Index: {}", &vin.vout));
         }
 
-        for vout in tx.vout {
+        for vout in &tx.vout {
             substreams::log::println(format!("Vout Value: {}", &vout.value));
-            txo_count += 1; // Increment the count for each transaction output
+            txo_count += 1;
+            tx_value += vout.value;
+
+            if let Some(script_pub_key) = &vout.script_pub_key {
+                if let Some(address) = script_pub_key.addresses.first() {
+                    let balance = address_map.entry(address.clone()).or_insert(0.0);
+                    *balance += vout.value;
+                }
+            }
         }
+
+        let sender = tx.vin.first().map(|vin| vin.txid.clone()).unwrap_or_default();
+        let receiver = tx.vout.first().and_then(|vout| vout.script_pub_key.as_ref().and_then(|spk| spk.addresses.first().cloned())).unwrap_or_default();
+
+        transactions.push(Transaction {
+            id: tx.hash.clone(),
+            sender,
+            receiver,
+            value: tx_value as i64,
+            block_number: block.height,
+            timestamp: block.time,
+        });
     }
+
+    let addresses: Vec<Address> = address_map.into_iter().map(|(id, balance)| Address {
+        id,
+        balance: balance as i64,
+        transactions: vec![],
+    }).collect();
 
     Ok(Txo {
         txo_count,
+        addresses,
+        transactions,
     })
 }
--- src/pb/bitcoin.template.rs
+++ src/pb/bitcoin.template.rs
@@ -4,5 +4,35 @@
 pub struct Txo {
     #[prost(int64, tag="1")]
     pub txo_count: i64,
+    #[prost(message, repeated, tag="2")]
+    pub addresses: ::prost::alloc::vec::Vec<Address>,
+    #[prost(message, repeated, tag="3")]
+    pub transactions: ::prost::alloc::vec::Vec<Transaction>,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Transaction {
+    #[prost(string, tag="1")]
+    pub id: ::prost::alloc::string::String,
+    #[prost(string, tag="2")]
+    pub sender: ::prost::alloc::string::String,
+    #[prost(string, tag="3")]
+    pub receiver: ::prost::alloc::string::String,
+    #[prost(int64, tag="4")]
+    pub value: i64,
+    #[prost(int64, tag="5")]
+    pub block_number: i64,
+    #[prost(int64, tag="6")]
+    pub timestamp: i64,
+}
+#[allow(clippy::derive_partial_eq_without_eq)]
+#[derive(Clone, PartialEq, ::prost::Message)]
+pub struct Address {
+    #[prost(string, tag="1")]
+    pub id: ::prost::alloc::string::String,
+    #[prost(int64, tag="2")]
+    pub balance: i64,
+    #[prost(message, repeated, tag="3")]
+    pub transactions: ::prost::alloc::vec::Vec<Transaction>,
 }
 // @@protoc_insertion_point(module)
